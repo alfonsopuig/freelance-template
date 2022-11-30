fdc1d96d709f8ef00af2672273508059c2eb300e
Alfonso Puig <13857079+alfonsopuig@users.noreply.github.com>
2022-11-30 12:47:42 +0100

    Second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 
 import localFont from '@next/font/local';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 const outfitFont = localFont({ src: '../public/fonts/Outfit-VariableFont_wght.woff2' });
 
